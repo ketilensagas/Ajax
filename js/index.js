895da3f47c5999049a7d55e3b2511cc4fb62a5a5
Ketilen Sagas <ketilensagas@hotmail.com>
2023-11-10 23:06:23 -0300

    validacao user

--- js/index.js
+++ js/index.js
@@ -1,29 +1,33 @@
 
-const url = 'https://api.github.com/users';
-const main = document.getElementById('main')
-let text = ''
+async function getUsers(username) {
+    const userListElement = document.getElementById('userList');
+    const url = `https://api.github.com/search/users?q=${username}+in:login`;
 
-document.getElementById('userInput').addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        getUser();
-    }
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-})
+        if (response.ok) {
+            userListElement.innerHTML = '';
 
-function getUser(user) {
-
-    fetch(`${url}/${user}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
+            data.items.forEach(user => {
+                const listItem = document.createElement('li');
+                listItem.textContent = user.login;
+                userListElement.appendChild(listItem);
+            });
+        } else {
+            throw new Error('Erro ao buscar usernames');
         }
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            text += `<p>${data.name} possui ${data.public_repos} repositórios públicos no Github como ${data.login}</p>`
-            main.innerHTML = text
-        })
-        .catch((error) => console.error('Erro:', error.message || error))
+    } catch (error) {
+        console.error('Ocorreu um erro:', error);
+    }
 }
 
+document.getElementById('searchInput').addEventListener('keypress', async function (e) {
+    if (e.key === 'Enter') {
+        const username = e.target.value.trim();
+        if (username !== '') {
+            getUsers(username);
+        }
+    }
+});
